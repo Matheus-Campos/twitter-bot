db3b28cab7ce5bad33ee151f9335942dd094e7d4
Lucas Vieira <lucas.filipe.vieira@gmail.com>
2018-07-04 21:40:38 -0300

    Escrevendo arquivos com analises.

--- tweet_analisys_new.py
+++ tweet_analisys_new.py
@@ -9,31 +9,32 @@ from sklearn.model_selection import cross_val_predict
 
 debug = False
 
+
 def filtraPalavra(palavra):
     # ignora palavras vazias
     if palavra == "":
         if debug:
-            print ("palavra vazia ignorada")
+            print("palavra vazia ignorada")
         return False
     # ignora qualquer palavra que comeca com http
     if palavra[:4] == "http":
         if debug:
-            print ("link ignorado")
+            print("link ignorado")
         return False
     # ignora qualquer palavra que tenha @ no inicio (referencia a outro usuario)
     if palavra[0] == "@":
         if debug:
-            print ("nome de usuario removido")
+            print("nome de usuario removido")
         return False
     # ignora RT (retwit)
     if palavra == "RT":
         if debug:
-            print ("removido tag RT")
+            print("removido tag RT")
         return False
     # ignora richtext
     if palavra[0] == "&":
         if debug:
-            print ("removido tag richtext")
+            print("removido tag richtext")
         return False
     # ignora hashtag
     if palavra[0] == "#":
@@ -41,6 +42,7 @@ def filtraPalavra(palavra):
             print("removido hashtag")
         return False
     return True
+
 
 # dataset = pd.read_csv('Tweets_Mg.csv',encoding='utf-8')
 def limpa_tweet(tweet):
@@ -51,47 +53,58 @@ def limpa_tweet(tweet):
     # remonta texto com lista de palavras
     return " ".join(lista_palavras)
 
+
+def analisa_sentimento(arq_name):
+    linhas = []
+    tweets = []
+    with open('output_' + arq_name + '.csv', 'r', encoding="UTF8") as arq:
+        reader = csv.reader(arq, delimiter='\t')
+        for row in reader:
+            try:
+                tweets.append(limpa_tweet(row[11]))
+                linhas.append(row[0:13])
+            except:
+                continue
+    # print(len(tweets))
+    # print(len(linhas))
+    freq_tweets = vectorizer.transform(tweets)
+    analise = modelo.predict(freq_tweets)
+    analise = analise.tolist()
+    # print(linhas)
+    for pos, linha in enumerate(linhas):
+        linhas[pos].append(analise[pos])
+    with open('output_' + arq_name + '_analisys.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+
+        for row in linhas:
+            writer.writerow(row)
+    return
+
+
+## -------- TREINO DO ALGORITMO ATRAVES DO MACHINE LEARNING --- NAO ALTERAR -----
 l_tweets = []
 l_classes = []
-with open('Modelo_base_barroso.csv','r',encoding="UTF8") as f:
+
+with open('Modelo_base_barroso.csv', 'r', encoding="UTF8") as f:
     reader = csv.reader(f, delimiter=';')
     for row in reader:
         l_tweets.append(limpa_tweet(row[11]))
         l_classes.append(row[13])
 
-print(l_tweets)
 vectorizer = CountVectorizer(analyzer="word")
 freq_tweets = vectorizer.fit_transform(l_tweets)
 modelo = MultinomialNB()
-modelo.fit(freq_tweets,l_classes)
-
-#{{Sessão destinada a inserção dos tweets da base de dados}}
-# testes2 = ['O militante Roberto Barroso está desrespeitando a constituição p/ tentar atrapalhar o gov. @MichelTemer e consequentemente, atrapalhar a recuperação do Brasil. Não bastaram as tentativas de golpe contra o Temer. O @SenadoFederal não reage, deveriam pedir o impeachment desse cara.']
+modelo.fit(freq_tweets, l_classes)
 
 freq_testes = vectorizer.transform(l_tweets)
-modelo.predict(freq_testes)
 
-#RESULTADO DA ANALISE
-print(modelo.predict(freq_testes))
-
-resultados = cross_val_predict(modelo, freq_tweets, l_classes, cv=10)
-print(metrics.accuracy_score(l_classes, resultados))
+# RESULTADO DA ANALISE
+# resultados = cross_val_predict(modelo, freq_tweets, l_classes, cv=10)
 
 
-sentimento =['positivo','negativo','neutro']
-print (metrics.classification_report(l_classes,resultados,sentimento))
+arquivos = [ 'barroso', 'carmen_lucia', 'celso', 'fachin', 'fux', 'gilmar', 'marco_aurelio', 'moraes', 'ricardo', 'stf',
+            'toffoli', 'weber']
 
-# --- Da erro por algum motivo ---
-# print(pd.crosstab(l_classes, resultados, rownames=['Real'], colnames=['Predito'], margins=True))
-
-
-# -- Utilizando Bigrams --
-vectorizer2 = CountVectorizer(ngram_range=(1,2))
-freq_tweets2 = vectorizer.fit_transform(l_tweets)
-modelo2 = MultinomialNB()
-modelo2.fit(freq_tweets2,l_classes)
-
-resultados2 = cross_val_predict(modelo2, freq_tweets2, l_classes, cv=10)
-print(metrics.accuracy_score(l_classes,resultados2))
-
-print (metrics.classification_report(l_classes,resultados2,sentimento))
+for i in arquivos:
+    # print(i)
+    analisa_sentimento(i)
